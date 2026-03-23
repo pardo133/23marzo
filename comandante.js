f8cbecadb9718564740adb884a74e76560f2e3e1
Mario <minmarmario@gmail.com>
2026-03-23 16:51:54 +0100

    rama mario cambios JS

---
+++ comandante.js
@@ -0,0 +1,16 @@
+const mario = "Mario";
+const destinos = ["marte", "jupiter", "saturno"];
+
+// 1. Añadimos comillas al ID
+const btnidentificar = document.querySelector("#btn-identificar");
+
+// 2. Añadimos la COMA después de "click"
+btnidentificar.addEventListener("click", () => {
+    document.querySelector("#nombre-id").innerText = mario;
+    
+    // Accedemos al segundo (índice 1)
+    document.querySelector("#destino-id").innerText = destinos[1];
+
+    // 3. Cambiamos miNombre por mario para que coincida con tu variable
+    console.log("Comandante identificado: " + mario);
+});
